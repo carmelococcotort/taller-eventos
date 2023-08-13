8e4cbe35444e86bbb5b06a5994e2585026e223e4
carmelococcotort <carmelococco1@gmail.com>
2023-08-13 17:53:07 -0300

    Problema

--- eventos.js
+++ eventos.js
@@ -1,5 +1,11 @@
 let div = document.querySelector("div");
+let saludarBtn = document.querySelector("button");
 
 div.addEventListener("click", function() {
   alert("Hola! Soy el div");
 });
+
+saludarBtn.addEventListener("click", function() {
+    event.stopPropagation(); // Detener la propagación del evento
+  alert("Hola!");
+  });
